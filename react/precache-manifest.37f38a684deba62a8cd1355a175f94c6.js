894c2012e8fe90b9dde90d56469f24d3e7dd8270
woai3c <411020382@qq.com>
2020-12-08 22:20:16 +0800

    a

---
+++ react/precache-manifest.37f38a684deba62a8cd1355a175f94c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d39091c066e4c92d802bef256336a95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "982104b9e9a6d640732c",
+    "url": "/static/css/2.7bfd32cb.chunk.css"
+  },
+  {
+    "revision": "1b6c23b1ec2c13d438a3",
+    "url": "/static/css/main.c9be9530.chunk.css"
+  },
+  {
+    "revision": "982104b9e9a6d640732c",
+    "url": "/static/js/2.6805efe1.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.6805efe1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e618aca508b57634faa",
+    "url": "/static/js/3.4af894f2.chunk.js"
+  },
+  {
+    "revision": "1b6c23b1ec2c13d438a3",
+    "url": "/static/js/main.0787dab1.chunk.js"
+  },
+  {
+    "revision": "83756ebb04dad146a7e8",
+    "url": "/static/js/runtime-main.ac63a986.js"
+  }
+]);
